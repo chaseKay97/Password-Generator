226fbc086a1d58f0f0cc6a95838434711e35dc12
chase kilpatrick <chasekilpatrick97@gmail.com>
2020-08-03 10:37:53 -0500

    added characters

--- Develop/script.js
+++ Develop/script.js
@@ -19,11 +19,12 @@ var confirmLowercase;
 // password characters 
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-",];
 //password numbers 
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 //alphabet
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //uppercase alphabet
 upperAlphabet= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 
 // Add event listener to generate button
