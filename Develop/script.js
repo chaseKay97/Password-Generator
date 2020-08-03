950ab9c144a55252b001581f21c1f8768f86b2c3
chase kilpatrick <chasekilpatrick97@gmail.com>
2020-08-03 11:37:47 -0500

    redesign the layout

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,14 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-  passwordText.value = password;
-
-}
-//options
-var enter;
-var confirmNumber;
-var confirmCharacter;
-var confirmUppercase;
-var confirmLowercase;
-
-// password characters 
-character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-",];
-//password numbers 
-number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-//alphabet
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//uppercase alphabet
-upperAlphabet= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-function writePassword(){
-  document.getElementById("")
-}
+//Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
