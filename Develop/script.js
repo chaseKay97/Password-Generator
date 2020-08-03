a4a675336b903005ffcf87f5618bb65c80bc382d
chase kilpatrick <chasekilpatrick97@gmail.com>
2020-08-03 10:03:26 -0500

    modified java script added most/all password choices

--- Develop/script.js
+++ Develop/script.js
@@ -10,10 +10,21 @@ function writePassword() {
 
 }
 //options
-var specialOne= ".,!/_"
-var upperCase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
-var lowerCase= "abcdefghijklmnopqrstuvwxyz"
-var numbers = "1234567890"
+var enter;
+var confirmNumber;
+var confirmCharacter;
+var confirmUppercase;
+var confirmLowercase;
+
+// password characters 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-",];
+//password numbers 
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//alphabet
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//uppercase alphabet
+upperAlphabet= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
